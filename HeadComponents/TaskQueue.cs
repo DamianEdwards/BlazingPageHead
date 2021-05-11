8336d45a179b92557ffe88d7cc592bb9b8cebca3
DamianEdwards <damian@damianedwards.com>
2021-05-11 14:11:40 -0700

    Fixed some issues & some clean-up
    Fixes #1

---
+++ HeadComponents/TaskQueue.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HeadComponents
+{
+    internal class TaskQueue
+    {
+        private SemaphoreSlim _semaphore;
+
+        public TaskQueue()
+        {
+            _semaphore = new SemaphoreSlim(1);
+        }
+
+        public async Task<T> Enqueue<T>(Func<Task<T>> taskGenerator)
+        {
+            await _semaphore.WaitAsync();
+            try
+            {
+                return await taskGenerator();
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+
+        public async Task<T> Enqueue<T>(Func<ValueTask<T>> taskGenerator)
+        {
+            await _semaphore.WaitAsync();
+            try
+            {
+                return await taskGenerator();
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+
+        public async Task Enqueue(Func<Task> taskGenerator)
+        {
+            await _semaphore.WaitAsync();
+            try
+            {
+                await taskGenerator();
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+
+        public async Task Enqueue(Func<ValueTask> taskGenerator)
+        {
+            await _semaphore.WaitAsync();
+            try
+            {
+                await taskGenerator();
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+    }
+}
--- HeadComponents/wwwroot/jsFunctions.js
+++ HeadComponents/wwwroot/jsFunctions.js
@@ -1,25 +1,27 @@
 // This is a JavaScript module that is loaded on demand. It can export any number of
 // functions, and may import other JavaScript modules if required.
 
-export function setDocumentTitle(title) {
-    document.title = title;
-}
-
 export function processHeadContent(contentElement, suffix) {
     let newContent = contentElement.childNodes;
+    var title;
     newContent.forEach(n => {
         if (n.nodeType === 1) { // Element node
             n.setAttribute("data-phm", "true");
             if (n.nodeName === "TITLE") {
-                setDocumentTitle(`${n.textContent}${suffix}`);
+                title = `${n.textContent}${suffix}`;
+                document.title = title;
             } else {
                 document.head.appendChild(n);
             }
         }
     });
+    if (title) {
+        return title;
+    }
 }
 
-export function resetHead() {
+export function resetHead(title) {
     let els = document.head.querySelectorAll("*[data-phm]");
     els.forEach(e => e.remove());
+    document.title = title;
 }
